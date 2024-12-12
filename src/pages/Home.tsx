84c2f483ebe7d528741c144cb4d168410b5639ba
Pamella Namie <pamella.nomura@gmail.com>
2024-12-11 22:50:47 -0300

    add button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
+
 export function Home() {
   return (
     <div>
       <h1>Olá mundo</h1>
+
+      <Button variant="contained" color="success">
+        Default
+      </Button>
     </div>
   );
 }
